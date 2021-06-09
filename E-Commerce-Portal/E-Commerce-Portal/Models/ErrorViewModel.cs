91af0b0ea88a9c693a08f8ce5eeb15d4d6b9b078
Arpit Gupta <zingahugane@gmail.com>
2021-06-09 11:49:23 +0530

    Created MVC App

---
+++ E-Commerce-Portal/E-Commerce-Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace E_Commerce_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
